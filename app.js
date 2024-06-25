fc2f9722495d286d436e924628c11cec8cb6bda6
Edith Harrison <edithharrison@ediths-laptop.lan>
2024-06-25 17:12:08 -0400

    Render views

--- app.js
+++ app.js
@@ -20,6 +20,7 @@ require("dotenv").config();
 const app = express();
 
 app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser(process.env.SESSION_SECRET));
 
